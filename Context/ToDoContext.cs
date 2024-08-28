5731b72aa032b9caf1def9dc6781c9c6b813293b
Khaled Hesham <khaledhesham007@gmail.com>
2024-08-29 02:04:48 +0300

    Commit Init

---
+++ Context/ToDoContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using To_Do_List.Models;
+
+namespace To_Do_List.Context
+{
+    public class ToDoContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=ToDoDB;Trusted_Connection=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public virtual DbSet <ToDo> Tasks { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //}
+    }
+}
---
+++ Models/ToDo.cs
@@ -0,0 +1,15 @@
+﻿namespace To_Do_List.Models
+{
+    public class ToDo : IComparable<ToDo>
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; } = string.Empty;
+        public bool IsDone { get; set; }
+        public int CompareTo(ToDo? other)
+        {
+            if (other == null) return 1;
+            return other.IsDone.CompareTo(this.IsDone);
+        }
+
+    }
+}
